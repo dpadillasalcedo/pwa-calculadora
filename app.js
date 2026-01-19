83751243a8e32168c26e9631ef8b042cdae35cf3
dpadillasalcedo <dpadillasalcedo@gmail.com>
2026-01-19 17:38:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -136,3 +136,75 @@ function calcularGCEco() {
     "<b>Gasto cardíaco: " + gc.toFixed(2) + " L/min</b>";
 }
 
+function calcularBloquePerfusion() {
+  const gc = parseFloat(document.getElementById("gc").value);
+  const hb = parseFloat(document.getElementById("hb").value);
+  const sao2 = parseFloat(document.getElementById("sao2").value);
+  const svo2 = parseFloat(document.getElementById("svo2").value);
+  const pao2 = parseFloat(document.getElementById("pao2").value);
+  const pvo2 = parseFloat(document.getElementById("pvo2").value);
+
+  const paco2 = parseFloat(document.getElementById("paco2").value);
+  const pvco2 = parseFloat(document.getElementById("pvco2").value);
+  const hco3a = parseFloat(document.getElementById("hco3a").value);
+  const hco3v = parseFloat(document.getElementById("hco3v").value);
+
+  const tam = parseFloat(document.getElementById("tam").value);
+  const pvc = parseFloat(document.getElementById("pvc").value);
+  const pia = parseFloat(document.getElementById("pia").value);
+  const pic = parseFloat(document.getElementById("pic").value);
+
+  const resultado = document.getElementById("resultadoPerfusion");
+
+  if (
+    [gc, hb, sao2, svo2, pao2, pvo2, paco2, pvco2,
+     hco3a, hco3v, tam, pvc, pia, pic].some(isNaN)
+  ) {
+    resultado.innerText = "Complete todos los campos";
+    return;
+  }
+
+  /* CONTENIDO DE O2 (mL/dL) */
+  const CaO2 = (1.34 * hb * (sao2 / 100)) + (0.003 * pao2);
+  const CvO2 = (1.34 * hb * (svo2 / 100)) + (0.003 * pvo2);
+  const deltaO2 = CaO2 - CvO2;
+
+  /* CONTENIDO DE CO2 (simplificado) */
+  const CaCO2 = (0.03 * paco2) + hco3a;
+  const CvCO2 = (0.03 * pvco2) + hco3v;
+  const deltaCO2 = CvCO2 - CaCO2;
+
+  /* TRANSPORTE Y CONSUMO */
+  const DO2 = gc * CaO2 * 10;
+  const VO2 = gc * deltaO2 * 10;
+  const REO2 = VO2 / DO2;
+
+  /* COCIENTE RESPIRATORIO */
+  const CR = deltaCO2 / deltaO2;
+
+  /* PERFUSIÓN */
+  const RVS = ((tam - pvc) / gc) * 80;
+  const PPR = tam - pia;
+  const PPC = tam - pic;
+
+  resultado.innerHTML =
+    "<b>OXIGENACIÓN</b><br>" +
+    "CaO₂: " + CaO2.toFixed(2) + " mL/dL<br>" +
+    "CvO₂: " + CvO2.toFixed(2) + " mL/dL<br>" +
+    "ΔO₂: " + deltaO2.toFixed(2) + " mL/dL<br><br>" +
+
+    "<b>CO₂</b><br>" +
+    "ΔCO₂: " + deltaCO2.toFixed(2) + "<br>" +
+    "Cociente respiratorio: " + CR.toFixed(2) + "<br><br>" +
+
+    "<b>TRANSPORTE</b><br>" +
+    "DO₂: " + DO2.toFixed(0) + " mL/min<br>" +
+    "VO₂: " + VO2.toFixed(0) + " mL/min<br>" +
+    "REO₂: " + (REO2 * 100).toFixed(1) + " %<br><br>" +
+
+    "<b>PERFUSIÓN</b><br>" +
+    "RVS: " + RVS.toFixed(0) + " dyn·s·cm⁻⁵<br>" +
+    "PPR: " + PPR.toFixed(0) + " mmHg<br>" +
+    "PPC: " + PPC.toFixed(0) + " mmHg";
+}
+
